c20bc05c9dde67895f819ec2aac155468db1ec9e
java21adakdipika <dipika.adak@aurionpro.com>
2023-07-23 13:38:28 +0530

    Customer Email

--- Customer Email/model/Customer.java
+++ Customer Email/model/Customer.java
@@ -10,24 +10,31 @@ public class Customer {
 	public int getCustId() {
 		return custId;
 	}
+	
 	public void setCustId(int custId) {
 		this.custId = custId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
